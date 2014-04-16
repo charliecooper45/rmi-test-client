b82706cacfa9a1238c6ca26d4534a70618d6d549
charliecooper45 <charliecooper98@gmail.com>
2014-04-16 16:17:22 +0100

    initial commit

---
+++ RecruitmentTestClient/src/TestServerInterface.java
@@ -0,0 +1,7 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+
+public interface TestServerInterface extends Remote {
+	public boolean connectToServer(String userName) throws RemoteException;
+}
